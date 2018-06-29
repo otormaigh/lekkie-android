7d773d3ec7976dc25cb9530ed873a794f6a13276
Elliot Tormey <elliot.tormey@gmail.com>
2018-06-29 21:23:42 +0100

    add better show/hide animation to outage list search bar

--- app/src/main/java/ie/pennylabs/lekkie/feature/list/OutageListController.kt
+++ app/src/main/java/ie/pennylabs/lekkie/feature/list/OutageListController.kt
@@ -24,26 +24,50 @@ import android.view.KeyEvent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintSet
+import androidx.constraintlayout.widget.ConstraintSet.*
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
-import com.bluelinelabs.conductor.ViewModelController
+import androidx.transition.TransitionManager
 import com.christianbahl.conductor.ConductorInjection
 import ie.pennylabs.lekkie.R
 import ie.pennylabs.lekkie.api.ApiService
+import ie.pennylabs.lekkie.arch.BaseController
 import ie.pennylabs.lekkie.data.model.OutageDao
 import ie.pennylabs.lekkie.toolbox.extension.hideKeyboard
 import ie.pennylabs.lekkie.toolbox.extension.showKeyboard
 import ie.pennylabs.lekkie.toolbox.extension.viewModelProvider
 import kotlinx.android.synthetic.main.controller_outage_list.view.*
-import kotlinx.coroutines.experimental.Job
+import kotlinx.coroutines.experimental.android.UI
 import kotlinx.coroutines.experimental.delay
 import kotlinx.coroutines.experimental.launch
 import javax.inject.Inject
 
-class OutageListController : ViewModelController(), TextWatcher {
+class OutageListController : BaseController(), TextWatcher {
   private val recyclerAdapter by lazy { OutageListRecyclerAdapter() }
   private val viewModel by viewModelProvider { OutageListViewModel(api, outageDao, geocoder) }
-  private val queryJob = Job()
+  private val constraintSetShowSearch by lazy {
+    ConstraintSet().apply {
+      clone(view?.parentViewGroup)
+      connect(R.id.ivSearch, START, PARENT_ID, START)
+      connect(R.id.ivSearch, END, R.id.etQuery, START)
+      connect(R.id.etQuery, START, R.id.ivSearch, END)
+      connect(R.id.etQuery, END, R.id.ivClear, START)
+      connect(R.id.ivClear, START, R.id.etQuery, END)
+      connect(R.id.ivClear, END, PARENT_ID, END)
+    }
+  }
+  private val constraintSetHideSearch by lazy {
+    ConstraintSet().apply {
+      clone(view?.parentViewGroup)
+      clear(R.id.ivSearch, START)
+      connect(R.id.ivSearch, END, PARENT_ID, END)
+      clear(R.id.etQuery, END)
+      connect(R.id.etQuery, START, PARENT_ID, END)
+      clear(R.id.ivClear, END)
+      connect(R.id.ivClear, START, PARENT_ID, END)
+    }
+  }
 
   @Inject
   lateinit var api: ApiService
@@ -59,6 +83,12 @@ class OutageListController : ViewModelController(), TextWatcher {
     ConductorInjection.inject(this)
     super.onAttach(view)
 
+    view.swipeRefresh.setOnRefreshListener {
+      view.swipeRefresh.isRefreshing = false
+      viewModel.fetchOutages()
+      hideSearch(view)
+    }
+
     view.rvOutages.apply {
       adapter = recyclerAdapter
       layoutManager = LinearLayoutManager(view.context)
@@ -67,48 +97,31 @@ class OutageListController : ViewModelController(), TextWatcher {
       })
     }
 
-    view.swipeRefresh.setOnRefreshListener {
-      view.etQuery.isEnabled = false
-      view.etQuery.hideKeyboard()
-      view.swipeRefresh.isRefreshing = false
-      viewModel.fetchOutages()
-    }
-
     view.ivSearch.setOnClickListener {
-      view.etQuery.requestFocus()
-      view.etQuery.isEnabled = true
-      view.etQuery.showKeyboard()
-      afterTextChanged(null)
-    }
+      TransitionManager.beginDelayedTransition(view.parentViewGroup)
+      constraintSetShowSearch.applyTo(view.parentViewGroup)
 
-    view.ivClear.setOnClickListener {
-      afterTextChanged(null)
-      view.etQuery.apply {
-        text.clear()
-        isEnabled = false
-        hideKeyboard()
-      }
-    }
-
-    view.etQuery.apply {
-      addTextChangedListener(this@OutageListController)
-      setOnKeyListener { view, _, keyEvent ->
-        when (keyEvent.keyCode) {
-          KeyEvent.KEYCODE_ENTER -> {
-            view.isEnabled = false
-            view.hideKeyboard()
-            viewModel.queryQao(view.etQuery.text.toString())
-            true
+      launch(UI, parent = onDetachJob) {
+        delay(300)
+        view.etQuery.apply {
+          requestFocus()
+          showKeyboard()
+          addTextChangedListener(this@OutageListController)
+          setOnKeyListener { _, _, keyEvent ->
+            when (keyEvent.keyCode) {
+              KeyEvent.KEYCODE_ENTER -> {
+                viewModel.queryQao(text.toString())
+                hideKeyboard()
+                true
+              }
+              else -> false
+            }
           }
-          else -> false
         }
       }
     }
-  }
 
-  override fun onDetach(view: View) {
-    super.onDetach(view)
-    queryJob.cancel()
+    view.ivClear.setOnClickListener { hideSearch(view) }
   }
 
   override fun afterTextChanged(text: Editable?) {
@@ -121,12 +134,26 @@ class OutageListController : ViewModelController(), TextWatcher {
   }
 
   override fun beforeTextChanged(text: CharSequence, p1: Int, p2: Int, p3: Int) {
+    // IGNORE
   }
 
   override fun onTextChanged(text: CharSequence, p1: Int, p2: Int, p3: Int) {
-    launch(queryJob) {
-      delay(200)
+    launch(onDetachJob) {
+      delay(300)
       viewModel.queryQao(text.toString())
+    }
+  }
+
+  private fun hideSearch(view: View) {
+    TransitionManager.beginDelayedTransition(view.parentViewGroup)
+    constraintSetHideSearch.applyTo(view.parentViewGroup)
+
+    launch(UI, parent = onDetachJob) {
+      afterTextChanged(null)
+      view.etQuery.apply {
+        text.clear()
+        hideKeyboard()
+      }
     }
   }
 }
